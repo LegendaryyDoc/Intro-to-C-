4d5b3e7e93284acf7ba019e2b55f8db95e244b2b
Nathan Knebusch <nateknebusch@gmail.com>
2019-01-31 17:55:25 -0800

    lots of errors but got to commit to get going

--- IntroCSharp/Program.cs
+++ IntroCSharp/Program.cs
@@ -6,57 +6,142 @@ using System.Threading.Tasks;
 
 namespace IntroCSharp
 {
-    class Program
-    {
+    class Chat
+    {     
         static void Main(string[] args)
         {
+            bool endChat = false;
+
             Console.WriteLine("This is the start of this random chat.");
-            Console.WriteLine("Enter some text");
 
-            String userInput = Console.ReadLine();
-
-            /*var replacements = new[] // weird stuff but works
+            while (endChat == false)
             {
-                new {Find = "Hello", Replace = "*****"},
-                new {Find = "name", Replace = "****"},
-                new {Find = "Yes", Replace = "***"},
-            };
+                string[] fileWordFilter = System.IO.File.ReadAllLines("TextFiles/Filter.txt"); // reading from file for filtered words
+                String FilteredWords = string.Join(" ", fileWordFilter); // used to store old words so can add onto the end of a file in blacklist
+                string[] fileJokes = System.IO.File.ReadAllLines("TextFiles/Jokes.txt"); // reading from file for filtered words
+                String oldJokes = string.Join(", ", fileJokes); // used to store old jokes
+                
+                Console.WriteLine("Enter some text");
 
-            foreach (var set in replacements)
-            {
-                userInput = userInput.Replace(set.Find, set.Replace);
-            }*/
+                String userInput = Console.ReadLine();
+                Console.WriteLine("");
+                args = userInput.Split(' ');
 
-            args = userInput.Split(' ');
+                for (int i = 0; i < args.Length; i++)
+                {
+                    /*    Checking for command    */
 
-            for(int i = 0; i < args.Length; i++)
-            {
-                bool helloWord = string.Equals(args[i], "hello", StringComparison.CurrentCultureIgnoreCase);
-                bool noWord = string.Equals(args[i], "no", StringComparison.CurrentCultureIgnoreCase);
-                bool yesWord = string.Equals(args[i], "yes", StringComparison.CurrentCultureIgnoreCase);
-                bool cantWord = string.Equals(args[i], "can't", StringComparison.CurrentCultureIgnoreCase);
+                    if (args[0] == "!") // if ! is the first thing put its a command
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan; // changes chat color to DarkCyan for commands
+                        if (args[i] == "commands") // displays a all available commands
+                        {
+                            args[i] = "Commands:\n" +
+                                      "- ! fly\n" +
+                                      "- ! rules\n" +
+                                      "- ! addblacklistedwords\n" +
+                                      "- ! endchat\n" +
+                                      "- ! joke\n" +
+                                      "- ! addnewjoke\n";
+                        }
 
-                if (helloWord == true)
-                {
-                    args[i] = "*****";
+                        else if (args[i] == "fly")
+                        {
+                            args[i] = "You cannot fly sorry to say...\n";
+                        }
+
+                        else if (args[i] == "rules")
+                        {
+                            args[i] = "Rules:\n" +
+                                      "- Be Respectful\n" +
+                                      "- Don't be a bot\n" +
+                                      "- NO SPAM\n";
+                        }
+
+                        else if (args[i] == "joke")
+                        {
+                            if (fileJokes.Length > 0)
+                            {
+                                Random rand = new Random();
+                                int randJoke = rand.Next(fileJokes.Length);
+
+                                Console.WriteLine(fileJokes[randJoke]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("No Jokes have been added try command addnewjoke to make your own!");
+                            }
+                        }
+
+                        else if (args[i] == "addnewjoke")
+                        {
+                            Console.WriteLine("Add a new joke to the list of jokes!");
+                            string jokeText = Console.ReadLine();
+
+                            if (oldJokes != null)
+                            {
+                                string oldAndNewJokes = oldJokes + jokeText;
+
+                                string[] newJokeList = oldAndNewJokes.Split(',');
+
+                                System.IO.File.WriteAllLines("TextFiles/Jokes.txt", newJokeList);
+                            }
+                            else
+                            {
+                                string[] newJokeList = jokeText.Split(',');
+
+                                System.IO.File.WriteAllLines("TextFiles/Jokes.txt", newJokeList);
+                            }
+                        }
+
+                        else if (args[i] == "addblacklistedwords") // adds another word to the filters
+                        {
+                            Console.WriteLine("Add to the list of black listed words for chat, put a space inbetween each of them");
+                            string text = Console.ReadLine(); // storing the users input into a string
+
+                            string oldAndNewWords = FilteredWords + " " + text; // adding the old and new words to same string so doesnt delete the old ones and instead adds onto the end of it
+
+                            string[] blackListedWords = oldAndNewWords.Split(' ');
+
+                            System.IO.File.WriteAllLines("TextFiles/Filter.txt", blackListedWords);
+
+                            Console.WriteLine("Words added to the list of not to be used");
+                        }
+
+                        else if (args[i] == "endchat")
+                        {
+                            endChat = true;
+                            Console.WriteLine("Leaving chat\n");
+                        }
+
+                        else if (args[i] != "!")
+                        {
+                            args[i] = "Sorry that is not possible use the ! command to see what commands you can use.\n";
+                        }
+                    }
+
+                    /*    Filtering out words    */
+                    else
+                    {
+                        for (int j = 0; j < fileWordFilter.Length; j++)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            bool word = string.Equals(args[i], fileWordFilter[j], StringComparison.CurrentCultureIgnoreCase);
+
+                            if (word == true)
+                            {
+                                args[i] = "****";
+                            }
+                        }
+                    }
                 }
-                if (noWord == true)
-                {
-                    args[i] = "**";
-                }
-                if (yesWord == true)
-                {
-                    args[i] = "*****";
-                }
-                if (cantWord)
-                {
-                    args[i] = "****";
-                }
+
+                userInput = string.Join(" ", args);
+
+                Console.WriteLine(userInput + "\n");
+                Console.ResetColor();
             }
-
-            userInput = string.Join(" ", args);
-
-            Console.WriteLine(userInput);
         }
+    
     }
 }
---
+++ MapEdit/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MapEdit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBoxItem_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_DragEnter(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Tiles_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            //(Tile_Options.SelectedItem as ListBoxItem).Content;
+            SolidColorBrush b = (SolidColorBrush)(Tile_Options.SelectedItem as ListBoxItem).Background;
+            /*var ee = (UIElement)e.Source;
+            int c = Grid.GetColumn(ee);
+            int r = Grid.GetRow(ee);*/
+
+            var label = e.Source as UIElement;
+            var row = Grid.GetRow(label);
+            var col = Grid.GetColumn(label);
+
+
+        }
+    }
+}
