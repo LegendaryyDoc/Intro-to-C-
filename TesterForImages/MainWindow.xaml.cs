696c35d4402378b6ca38b11bdf5317724e725379
Nathan Knebusch <nateknebusch@gmail.com>
2019-02-21 18:00:41 -0800

    added onto my testerforimages so that it can load an image and use it as a ImageBrush from file, and added a grid that can change still needs labels for copy pasting id numbers

--- TesterForImages/MainWindow.xaml.cs
+++ TesterForImages/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 /*-------------------------------------------------------------------*/
 /*------------------------- TO DO -----------------------------------*/
@@ -44,10 +45,80 @@ namespace TesterForImages
     /// </summary>
     public partial class MainWindow : Window
     {
+        void imageFileNameSaver() // adds files into a file to be loaded back into wpf as a image source
+        { 
+            using (StreamWriter a = File.AppendText("ImageSources.txt"))
+            {
+                string[] b = Directory.GetFiles("C:/Users/s189065/source/repos/IntroCSharp/TesterForImages/bin/Debug/tiles");
+                foreach(string dir in b)
+                {
+                    a.WriteLine(dir);
+                }
+                a.Close();
+            } 
+        }
+
+        void imageSourceLoader() // loads stuff from file as a image source
+        {
+            string[] textReader = System.IO.File.ReadAllLines("ImageSources.txt");
+            BitmapImage[] myBitMapImage = new BitmapImage[textReader.Length];
+            Setter tileHolder = new Setter();
+            Border LBIBorder = new Border();
+            ImageBrush backGroundColor = new ImageBrush();
+
+            for (int i = 0; i < textReader.Length; i++)
+            {
+                if (textReader[i] != null)
+                {
+                    myBitMapImage[i] = new BitmapImage();
+                    myBitMapImage[i].BeginInit();
+                    myBitMapImage[i].UriSource = new Uri(textReader[i]);
+                    myBitMapImage[i].EndInit();
+
+                    ImageHolder.ImageSource = myBitMapImage[i];
+                } // need to make it so will auto make all the files added
+            }
+        }
+
+        void gridCreator() // need to add labels to each tile in grid
+        {
+            int GridRows = 10;
+            int GridCols = 10;
+            for (int i = 0; i < GridRows; i++)
+            {
+                RowDefinition gridRow = new RowDefinition();
+                gridRow.Height = new GridLength(TileMap.Height / GridRows);
+
+                TileMap.RowDefinitions.Add(gridRow);
+            }
+
+            for (int i = 0; i < GridCols; i++)
+            {
+                ColumnDefinition gridCol = new ColumnDefinition();
+                gridCol.Width = new GridLength(TileMap.Width / GridCols);
+
+                TileMap.ColumnDefinitions.Add(gridCol);
+            }
+
+            for(int i = 0; i < GridRows; i++)
+            {
+                Label nameLabel = new Label();
+                for(int j = 0; j < GridCols; j++)
+                {
+                    nameLabel.Content = i.ToString() + "," + j.ToString(); 
+                }
+                
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
-            
+
+            imageSourceLoader();
+
+            //imageFileNameSaver();
+
+            gridCreator();
         }
     }
 }
